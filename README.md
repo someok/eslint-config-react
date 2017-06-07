0f5149bd173ef02c92ad9d29a3f7a4761f4efe62
JX Wang <wjianxu@gmail.com>
2017-06-07 20:04:39 +0800

    从 eslint-config-someok 迁移过来

---
+++ README.md
@@ -0,0 +1,12 @@
+# eslint-config-react
+
+我的常用 eslint 配置，之所以搞出这么个配置是为了方便在多个项目中引入，
+而不需要在每个项目中拷贝一份。
+
+此配置依赖 `airbnb` 的配置，并做了一些简单的调整。
+
+## 配置查看方式
+
+如果想查看某项目下合并后的 eslint 配置，可以运行如下命令：
+
+> npm run eslint -- --print-config .eslintrc.json 
---
+++ index.js
@@ -0,0 +1,81 @@
+/*
+ * Created by wjx on 2017/4/25.
+ */
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": [
+        "airbnb"
+    ],
+    "env": {
+        "es6": true,
+        "browser": true,
+        "amd": true,
+        "node": true,
+        "jquery": true,
+        "mocha": true
+    },
+    rules: {
+        // eslint default rule
+        "arrow-body-style": 0,
+        "arrow-parens": 0,
+        "comma-dangle": 0,
+        "linebreak-style": 0,
+        "global-require": 0,
+        "indent": [
+            2,
+            4,
+            {
+                "SwitchCase": 1,
+                "VariableDeclarator": 1
+            }
+        ],
+        "no-console": 0,
+        "no-mixed-operators": 0,
+        "no-plusplus": 0,
+        "no-underscore-dangle": 0,
+        "no-unused-expressions": [
+            2,
+            {
+                "allowShortCircuit": true,
+                "allowTernary": true
+            }
+        ],
+        "object-curly-spacing": 0,
+        "padded-blocks": 0,
+        "prefer-template": 0,
+        "space-before-function-paren": [
+            2,
+            "never"
+        ],
+
+        // 允许只有一个非 default 的 export
+        "import/prefer-default-export": 0,
+
+        // 允许 js 扩展名
+        "react/jsx-filename-extension": 0,
+        "react/jsx-indent": [
+            2,
+            4
+        ],
+        "react/jsx-indent-props": [
+            2,
+            4
+        ],
+        "react/jsx-space-before-closing": 0,
+        "react/jsx-tag-spacing": 0,
+        "react/sort-comp": [
+            "error",
+            {
+                "order": [
+                    "static-methods",
+                    "lifecycle",
+                    "everything-else",
+                    "/^render.+$/",
+                    "render"
+                ]
+            }
+        ]
+
+    }
+
+};
